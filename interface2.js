1097aa1ee921abb2c2d3311e069d37f23ab9661d
Diri <Diri>
2016-02-24 17:51:24 -0500

    almost there

--- interface2.js
+++ interface2.js
@@ -13,36 +13,46 @@ var etat = {
       "to": "90221F212A4200001AA",
       "date": "2016 01 12 20:15:42",
       "msg": "What are u doin ?." } ],
-  "yp": {
-    "AF22111212232211122": {"name": "Jean Fanchon"},
-    "90221F212A4200001AA": {"name": "Bob"} }
+  "yp": {  "AF22111212232211122": {"name": "Jean Fanchon"},"90221F212A4200001AA": {"name": "Bob"} }
 };
 
-$("#b1").click( function (){
-etat.inbox.forEach( function(msg){
-  $("#id").append(
-    $("<h1>").text(msg.msg));
-    $("#id").append(
-      $("<h1>").text(etat.yp[msg.from].name));
-      $("#id").append(
-        $("<h1>").text(msg.date));
+
+
+$("#inbox").click( function (){
+etat.inbox.forEach( function(msg,x){
+  if (x<2 ) {
+  $("#showinbox").append(
+    $("<h5>").text(msg.msg));
+    $("#showinbox").append(
+      $("<h5>").text(etat.yp[msg.from].name));
+      $("#showinbox").append(
+        $("<h5>").text(msg.date));
+
+}
 });
 });
-$("#b2").click( function (){
+$("#outbox").click( function (){
 etat.outbox.forEach( function(msg){
-  $("#id").append(
-    $("<h1>").text(msg.date));
+  $("#showoutbox").append(
+    $("<h3>").text(msg.date));
 
-    $("#id").append(
-      $("<h1>").text(etat.yp[msg.to].name));
-      $("#id").append(
-        $("<h1>").text(msg.msg));
+    $("#showoutbox").append(
+      $("<h3>").text(etat.yp[msg.to].name));
+      $("#showoutbox").append(
+        $("<h3>").text(msg.msg));
 
 });
 });
-
+$("#contact").click( function (){
+  etat.outbox.forEach( function(msg){
+  $("#showcontact").append(
+    $("<h3>").text(etat.yp[msg.to].name));
+    etat.inbox.forEach( function(msg){
+        $("#showcontact").append(
+      $("<h5>").text(etat.yp[msg.from].name));
+  });
+});
+});
 $("#b4").click(function(){
-  etat.outbox.push({to:$("#@").text(),msg:$("#msg").text(),date:new Date()});
-var new ("#b2")=("#b4");
-console.log(etat.outbox);
+  etat.outbox.append({"to":$("#@").text(),"msg":$("#msg").text(),"date":new Date()});
 });
